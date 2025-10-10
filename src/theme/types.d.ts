b84937d0ea53059cd8914b6fd69e8e3be30859ae
Stefano <stefanov.santos@gmail.com>
2025-10-10 12:32:26 -0300

    refactor(types): remove unnecessary comment from theme augmentation

--- src/theme/types.d.ts
+++ src/theme/types.d.ts
@@ -1,7 +1,6 @@
 import "@mui/material/styles";
 import "@mui/x-data-grid/themeAugmentation";
 
-// 👇 Extends MUI theme interfaces to recognize our custom structure
 declare module "@mui/material/styles" {
   interface Theme {
     cssVariables?: boolean;
