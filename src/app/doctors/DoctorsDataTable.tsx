270922f5a1609e6b3a4fc84d8ae69eda13b0c4e2
Stefano <stefanov.santos@gmail.com>
2025-10-17 11:27:52 -0300

    fix(DoctorsDataTable): update import path for MockDoctorsData to use utils

--- src/app/doctors/DoctorsDataTable.tsx
+++ src/app/doctors/DoctorsDataTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 import DataTableShell from "@/components/DataTableShell/DataTableShell";
-import { mDoctorsColumns, mDoctorsRows } from "./MockDoctorsData";
+import { mDoctorsColumns, mDoctorsRows } from "../../utils/MockDoctorsData";
 
 export default function DoctorsDataTable() {
   return <DataTableShell columns={mDoctorsColumns} rows={mDoctorsRows} />;
